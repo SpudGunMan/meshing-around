ffe03837cb07249015c3878682c93fa0f9bb1e1e
SpudGunMan <kellykeeton@yahoo.com>
2024-06-19 00:46:00 -0700

    Update mesh-bot.py

--- mesh-bot.py
+++ mesh-bot.py
@@ -19,7 +19,7 @@ interface = meshtastic.serial_interface.SerialInterface() #serial interface
 
 trap_list = ("ping","ack","testing","pong","motd","help","sun","solar","hfcond","lheard") #A list of strings to trap and respond to
 welcome_message = "MeshBot, here for you like a friend who is not. Try sending: ping @foo  or, help"
-help_message = "Commands are: ping, ack, motd, sun, solar, hfcond. Use 'motd $foo' to set MOTD."
+help_message = "Commands are: ping, ack, motd, sun, solar, hfcond, lheard. Use 'motd $foo' to set MOTD."
 RESPOND_BY_DM_ONLY = True # Set to True to respond messages via DM only (keeps the channel clean)
 MOTD = "Thanks for using PongBOT! Have a good day!" # Message of the Day
 
