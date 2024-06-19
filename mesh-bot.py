cc87be8c796c6806df0a6210fa5e3b9f12ef1bf2
SpudGunMan <kellykeeton@yahoo.com>
2024-06-18 22:54:32 -0700

    Update mesh-bot.py

--- mesh-bot.py
+++ mesh-bot.py
@@ -183,8 +183,10 @@ def get_node_list():
         for node in interface.nodes.values():
             #ignore own
             if node['num'] != myNodeNum:
-                node_list.append(get_name_from_number(node['num']))
-                
+                node_list.append([get_name_from_number(node['num']),node['lastHeard']])
+        node_list.sort(key=lambda x: x[1],reverse=True)
+        
+
         #return only the last 5 nodes
         return node_list[:5]
     else:
