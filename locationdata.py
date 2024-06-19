684efc470e842a1d3541aaff177f0b0b7e23d8b2
SpudGunMan <kellykeeton@yahoo.com>
2024-06-19 15:25:26 -0700

    Update locationdata.py

--- locationdata.py
+++ locationdata.py
@@ -5,7 +5,7 @@ from geopy.geocoders import Nominatim # pip install geopy
 
 def where_am_i(lat=0, lon=0):
     # initialize Nominatim API
-    geolocator = Nominatim(user_agent="geoapiExercises")
+    geolocator = Nominatim(user_agent="mesh-bot")
     
     location = geolocator.reverse(lat+","+lon)
     address = location.raw['address']
