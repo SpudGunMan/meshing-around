c54ab7d06341f75a77db85fc8d9ae91e8bc279db
SpudGunMan <kellykeeton@yahoo.com>
2024-06-19 14:57:55 -0700

    Update mesh-bot.py

--- mesh-bot.py
+++ mesh-bot.py
@@ -216,7 +216,7 @@ def get_node_location(number):
         for node in interface.nodes.values():
             if number == node['num']:
                 if 'position' in node:
-                    print (f"DEBUG: {node['position']}")
+                    #print (f"DEBUG: {node['position']}")
                     latitude = node['position']['latitude']
                     longitude = node['position']['longitude']
                     print (f"System: location data for {number} is {latitude},{longitude}")
