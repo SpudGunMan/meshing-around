02ecb680af3814a56ad53d578fa64c3b3e72e79f
SpudGunMan <kellykeeton@yahoo.com>
2024-06-19 21:29:26 -0700

    Update solarconditions.py

--- solarconditions.py
+++ solarconditions.py
@@ -112,10 +112,10 @@ def get_moon(lat=0, lon=0):
     moon_table['illumination'] = moon.phase
     moon_table['azimuth'] = moon.az
     moon_table['altitude'] = moon.alt
-    moon_table['rise_time'] = ephem.localtime(obs.next_rising(moon))
-    moon_table['set_time'] = ephem.localtime(obs.next_rising(moon))
-    moon_table['next_full_moon'] = ephem.localtime(obs.next_rising(moon))
-    moon_table['next_new_moon'] = ephem.localtime(obs.next_rising(moon))
+    moon_table['rise_time'] = ephem.localtime(obs.next_rising(moon)).strftime('%a %d %I:%M')
+    moon_table['set_time'] = ephem.localtime(obs.next_rising(moon)).strftime('%a %d %I:%M')
+    moon_table['next_full_moon'] = ephem.localtime(obs.next_rising(moon)).strftime('%a %d %I:%M')
+    moon_table['next_new_moon'] = ephem.localtime(obs.next_rising(moon)).strftime('%a %d %I:%M')
 
     moon_data = f"Moon Rise:" + moon_table['rise_time'] + " Set:" + moon_table['set_time'] + "\nMoon Phase:" \
         + moon_table['phase'] + " @:" + str('{0:.2f}'.format(moon_table['illumination'])) + "%" \
