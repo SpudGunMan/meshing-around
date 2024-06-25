3bb255d52bfc2218fd3390ab2400eb18348b3dd8
David Bures <12524436+PiDiBi@users.noreply.github.com>
2024-06-25 14:19:54 -0700

    add wxc for metric weather

--- locationdata.py
+++ locationdata.py
@@ -79,11 +79,11 @@ def get_tide(lat=0, lon=0):
     tide_string = tide_string[:-1]
     return tide_string
     
-def get_weather(lat=0, lon=0):
+def get_weather(lat=0, lon=0, unit = 0):
     weather = ""
     if float(lat) == 0 and float(lon) == 0:
         return NO_DATA_NOGPS
-    weather_url = "https://forecast.weather.gov/MapClick.php?FcstType=text&lat=" + str(lat) + "&lon=" + str(lon)
+    weather_url = "https://forecast.weather.gov/MapClick.php?FcstType=text&lat=" + str(lat) + "&lon=" + str(lon) + "&unit=" + str(unit)
     print(f"{log_timestamp()} weather_url: {weather_url}")
 
     try:
