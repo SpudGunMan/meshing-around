a1734aa5231be7b2461c54e37c9b48b37fab28d7
David Bures <12524436+PiDiBi@users.noreply.github.com>
2024-06-25 14:30:55 -0700

    refactoring spaces

--- locationdata.py
+++ locationdata.py
@@ -1,11 +1,12 @@
+from log import *
+
 # helper functions to use location data
 # K7MHI Kelly Keeton 2024
 
-from geopy.geocoders import Nominatim # pip install geopy
-import maidenhead as mh # pip install maidenhead
-import requests # pip install requests
-import bs4 as bs # pip install beautifulsoup4
-from log import *
+from geopy.geocoders import Nominatim  # pip install geopy
+import maidenhead as mh  # pip install maidenhead
+import requests  # pip install requests
+import bs4 as bs  # pip install beautifulsoup4
 
 TIMEOUT_DURATION = 10
 NO_DATA_NOGPS = "no location data: does your device have GPS?"
@@ -17,28 +18,28 @@ def where_am_i(lat=0, lon=0):
         return NO_DATA_NOGPS
     # initialize Nominatim API
     geolocator = Nominatim(user_agent="mesh-bot")
-    
-    location = geolocator.reverse(lat + "," + lon)
+
+    location = geolocator.reverse(lat + ", " + lon)
     address = location.raw['address']
     address_components = ['house_number', 'road', 'city', 'state', 'postcode', 'county', 'country']
     whereIam += ' '.join([address.get(component, ', ') for component in address_components if component in address])
 
     grid = mh.to_maiden(float(lat), float(lon))
-    whereIam += " Grid:" + grid
-    
+    whereIam += " Grid: " + grid
+
     return whereIam
 
 def get_tide(lat=0, lon=0):
     station_id = ""
     if float(lat) == 0 and float(lon) == 0:
         return NO_DATA_NOGPS
-    
+
     station_lookup_url = "https://api.tidesandcurrents.noaa.gov/mdapi/prod/webapi/tidepredstations.json?lat=" + str(lat) + "&lon=" + str(lon) + "&radius=50"
     print(f"{log_timestamp()} station_lookup_url: {station_lookup_url}")
-    
+
     try:
         station_data = requests.get(station_lookup_url, timeout=TIMEOUT_DURATION)
-        if(station_data.ok):
+        if station_data.ok:
             station_json = station_data.json()
         else:
             return ERROR_FETCHING_DATA
@@ -46,24 +47,24 @@ def get_tide(lat=0, lon=0):
         return ERROR_FETCHING_DATA
 
     station_id = station_json['stationList'][0]['stationId']
-    
-    station_url="https://tidesandcurrents.noaa.gov/noaatidepredictions.html?id=" + station_id
+
+    station_url = "https://tidesandcurrents.noaa.gov/noaatidepredictions.html?id=" + station_id
     print(f"{log_timestamp()} station_url: {station_url}")
-    
+
     try:
         station_data = requests.get(station_url, timeout=TIMEOUT_DURATION)
-        if(not station_data.ok):
+        if not station_data.ok:
             return ERROR_FETCHING_DATA
     except (requests.exceptions.RequestException):
         return ERROR_FETCHING_DATA
-    
-    #extract table class="table table-condensed"
+
+    # extract table class="table table-condensed"
     soup = bs.BeautifulSoup(station_data.text, 'html.parser')
     table = soup.find('table', class_='table table-condensed')
 
-    #extract rows
+    # extract rows
     rows = table.find_all('tr')
-    #extract data from rows
+    # extract data from rows
     tide_data = []
     for row in rows:
         row_text = ""
@@ -75,10 +76,10 @@ def get_tide(lat=0, lon=0):
     tide_string = ""
     for data in tide_data:
         tide_string += data + "\n"
-    #trim off last newline
+    # trim off last newline
     tide_string = tide_string[:-1]
     return tide_string
-    
+
 def get_weather(lat=0, lon=0, unit=0):
     weather = ""
     if float(lat) == 0 and float(lon) == 0:
@@ -88,21 +89,21 @@ def get_weather(lat=0, lon=0, unit=0):
 
     try:
         weather_data = requests.get(weather_url, timeout=TIMEOUT_DURATION)
-        if(not weather_data.ok):
+        if not weather_data.ok:
             return ERROR_FETCHING_DATA
     except (requests.exceptions.RequestException):
         return ERROR_FETCHING_DATA
 
     soup = bs.BeautifulSoup(weather_data.text, 'html.parser')
     table = soup.find('div', id="detailed-forecast-body")
-    
+
     if table is None:
         return "no weather data found on NOAA for your location"
 
-    #get rows
+    # get rows
     rows = table.find_all('div', class_="row")
-    
-    #extract data from rows
+
+    # extract data from rows
     for index, row in enumerate(rows):
         # shrink the text
         # Define a dictionary to map the long form to the short form
@@ -143,14 +144,14 @@ def get_weather(lat=0, lon=0, unit=0):
         for long_form, short_form in replace_dict.items():
             line = line.replace(long_form, short_form)
 
-        #only grab a few days of weather
-        
+        # only grab a few days of weather
+
         weather += line + "\n"
-        
+
         if index > 0:
             break
-        
-    #trim off last newline
+
+    # trim off last newline
     weather = weather[:-1]
 
     return weather
--- mesh_bot.py
+++ mesh_bot.py
@@ -1,34 +1,36 @@
+from datetime import datetime
+from log import *
+
 #!/usr/bin/env python3
 # Meshtastic Autoresponder MESH Bot
 # K7MHI Kelly Keeton 2024
 
-import signal # for catching CTL+C
-from pubsub import pub # pip install pubsub
-import meshtastic.serial_interface #pip install meshtastic
+import signal  # for catching CTL+C
+from pubsub import pub  # pip install pubsub
+import meshtastic.serial_interface  # pip install meshtastic
 import meshtastic.tcp_interface
 import meshtastic.ble_interface
-from datetime import datetime
 
-from solarconditions import * # from the spudgunman/meshing-around repo
-from locationdata import * # from the spudgunman/meshing-around repo
-from bbstools import * # from the spudgunman/meshing-around repo
+from solarconditions import *  # from the spudgunman/meshing-around repo
+from locationdata import *  # from the spudgunman/meshing-around repo
+from bbstools import *  # from the spudgunman/meshing-around repo
 
-from log import *
 
 # Uncomment the interface you want to use depending on your device connection
-interface = meshtastic.serial_interface.SerialInterface() #serial interface
-#interface=meshtastic.tcp_interface.TCPInterface(hostname="192.168.0.1") # IP of your device
-#interface=meshtastic.ble_interface.BLEInterface("AA:BB:CC:DD:EE:FF") # BLE interface
+interface = meshtastic.serial_interface.SerialInterface()  # serial interface
+# interface=meshtastic.tcp_interface.TCPInterface(hostname="192.168.0.1") # IP of your device
+# interface=meshtastic.ble_interface.BLEInterface("AA:BB:CC:DD:EE:FF") # BLE interface
 
-#A list of strings to trap and respond to
-trap_list = ("ping", "ack", "testing", "pong", "motd", "help", "sun", "solar", "hfcond", "lheard", "whereami", "tide", "moon", "wx", "wxc", "joke")
+# A list of strings to trap and respond to
+trap_list = ("ping", "ack", "testing", "pong", "motd", "help", "sun", "solar", "hfcond", "lheard", "whereami", "tide",
+             "moon", "wx", "wxc", "joke")
 
 welcome_message = "MeshBot, here for you like a friend who is not. Try sending: ping @foo  or, help"
 help_message = "Commands are: ack, hfcond, joke, Lheard, moon, motd, ping, solar, sun, tide, whereami, wx"
-MOTD = "Thanks for using PongBOT! Have a good day!" # Message of the Day
-RESPOND_BY_DM_ONLY = True # Set to True to respond messages via DM only (keeps the channel clean)
+MOTD = "Thanks for using PongBOT! Have a good day!"  # Message of the Day
+RESPOND_BY_DM_ONLY = True  # Set to True to respond messages via DM only (keeps the channel clean)
 
-#Get the node number of the device, check if the device is connected
+# Get the node number of the device, check if the device is connected
 try:
     myinfo = interface.getMyNodeInfo()
     myNodeNum = myinfo['num']
@@ -36,10 +38,11 @@ except Exception as e:
     print(f"System: Critical Error script abort. {e}")
     exit()
 
-def auto_response(message,snr,rssi,hop,message_from_id):
-    #Auto response to messages
+
+def auto_response(message, snr, rssi, hop, message_from_id):
+    # Auto response to messages
     if "ping" in message.lower():
-        #Check if the user added @foo to the message
+        # Check if the user added @foo to the message
         if "@" in message:
             if hop == "Direct":
                 bot_response = "PONG, " + f"SNR:{snr} RSSI:{rssi}" + " and copy: " + message.split("@")[1]
@@ -60,7 +63,7 @@ def auto_response(message,snr,rssi,hop,message_from_id):
     elif "pong" in message.lower():
         bot_response = "PING!!"
     elif "motd" in message.lower():
-        #check if the user wants to set the motd by using $
+        # check if the user wants to set the motd by using $
         if "$" in message:
             motd = message.split("$")[1]
             global MOTD
@@ -72,7 +75,7 @@ def auto_response(message,snr,rssi,hop,message_from_id):
         bot_response = help_message
     elif "sun" in message.lower():
         location = get_node_location(message_from_id)
-        bot_response = get_sun(str(location[0]),str(location[1]))
+        bot_response = get_sun(str(location[0]), str(location[1]))
     elif "hfcond" in message.lower():
         bot_response = hf_band_conditions()
     elif "solar" in message.lower():
@@ -81,15 +84,15 @@ def auto_response(message,snr,rssi,hop,message_from_id):
         bot_response = "lheard: " + str(get_node_list())
     elif "whereami" in message.lower():
         location = get_node_location(message_from_id)
-        where = where_am_i(str(location[0]),str(location[1]))
+        where = where_am_i(str(location[0]), str(location[1]))
         bot_response = where
     elif "tide" in message.lower():
         location = get_node_location(message_from_id)
-        tide = get_tide(str(location[0]),str(location[1]))
+        tide = get_tide(str(location[0]), str(location[1]))
         bot_response = tide
     elif "moon" in message.lower():
         location = get_node_location(message_from_id)
-        moon = get_moon(str(location[0]),str(location[1]))
+        moon = get_moon(str(location[0]), str(location[1]))
         bot_response = moon
     elif "wxc" in message.lower():
         location = get_node_location(message_from_id)
@@ -103,16 +106,17 @@ def auto_response(message,snr,rssi,hop,message_from_id):
         bot_response = tell_joke()
     else:
         bot_response = "I'm sorry, I'm afraid I can't do that."
-    
+
     return bot_response
-    
+
+
 def onReceive(packet, interface):
     # receive a packet and process it, main instruction loop
 
     # print the packet for debugging
-    #print("Packet Received")
-    #print(packet) # print the packet for debugging
-    #print("END of packet \n")
+    # print("Packet Received")
+    # print(packet) # print the packet for debugging
+    # print("END of packet \n")
     message_from_id = 0
     snr = 0
     rssi = 0
@@ -128,7 +132,7 @@ def onReceive(packet, interface):
                 channel_number = packet['channel']
             else:
                 channel_number = 0
-        
+
             # check if the packet has a hop count flag use it
             if packet.get('hopsAway'):
                 hop_away = packet['hopsAway']
@@ -139,7 +143,7 @@ def onReceive(packet, interface):
                     hop_limit = packet['hopLimit']
                 else:
                     hop_limit = 0
-                
+
                 if packet.get('hopStart'):
                     hop_start = packet['hopStart']
                 else:
@@ -153,48 +157,57 @@ def onReceive(packet, interface):
                     hop_count = hop_away
                 else:
                     hop_count = hop_start - hop_limit
-                    #print (f"calculated hop count: {hop_start} - {hop_limit} = {hop_count}")
+                    # print (f"calculated hop count: {hop_start} - {hop_limit} = {hop_count}")
 
                 hop = f"{hop_count} hops"
-            
+
             # If the packet is a DM (Direct Message) respond to it, otherwise validate its a message for us
             if packet['to'] == myNodeNum:
                 if messageTrap(message_string):
-                    print(f"{log_timestamp()} Received DM: {message_string} on Channel: {channel_number} From: {get_name_from_number(message_from_id)}")
+                    print(
+                        f"{log_timestamp()} Received DM: {message_string} on Channel: {channel_number} From: {get_name_from_number(message_from_id)}")
                     # respond with a direct message
-                    send_message(auto_response(message_string,snr,rssi,hop,message_from_id),channel_number,message_from_id)
-                else: 
-                    #respond with welcome message
-                    print(f"{log_timestamp()} Ignoring DM: {message_string} From: {get_name_from_number(message_from_id)}")
-                    send_message(welcome_message,channel_number,message_from_id)
+                    send_message(auto_response(message_string, snr, rssi, hop, message_from_id), channel_number,
+                                 message_from_id)
+                else:
+                    # respond with welcome message
+                    print(
+                        f"{log_timestamp()} Ignoring DM: {message_string} From: {get_name_from_number(message_from_id)}")
+                    send_message(welcome_message, channel_number, message_from_id)
             else:
                 if messageTrap(message_string):
-                    print(f"{log_timestamp()} Received On Channel {channel_number}: {message_string} From: {get_name_from_number(message_from_id)}")
+                    print(
+                        f"{log_timestamp()} Received On Channel {channel_number}: {message_string} From: {get_name_from_number(message_from_id)}")
                     if RESPOND_BY_DM_ONLY:
                         # respond to channel message via direct message to keep the channel clean
-                        send_message(auto_response(message_string,snr,rssi,hop,message_from_id),channel_number,message_from_id)
+                        send_message(auto_response(message_string, snr, rssi, hop, message_from_id), channel_number,
+                                     message_from_id)
                     else:
                         # or respond to channel message on the channel itself
-                        send_message(auto_response(message_string,snr,rssi,message_from_id),channel_number,0)
+                        send_message(auto_response(message_string, snr, rssi, message_from_id), channel_number, 0)
                 else:
-                    print(f"{log_timestamp()} System: Ignoring incoming channel {channel_number}: {message_string} From: {get_name_from_number(message_from_id)}")
-                
+                    print(
+                        f"{log_timestamp()} System: Ignoring incoming channel {channel_number}: {message_string} From: {get_name_from_number(message_from_id)}")
+
     except KeyError as e:
         print(f"System: Error processing packet: {e}")
-        print(packet) # print the packet for debugging
+        print(packet)  # print the packet for debugging
         print("END of packet \n")
-        
+
+
 def messageTrap(msg):
-    #Check if the message contains a trap word
-    message_list=msg.split(" ")
+    # Check if the message contains a trap word
+    message_list = msg.split(" ")
     for m in message_list:
         for t in trap_list:
             if t.lower() == m.lower():
                 return True
     return False
 
+
 def decimal_to_hex(decimal_number):
     return f"!{decimal_number:08x}"
+
 
 def get_name_from_number(number, type='long'):
     name = ""
@@ -209,15 +222,16 @@ def get_name_from_number(number, type='long'):
             else:
                 pass
         else:
-            name =  str(decimal_to_hex(number))  # If long name not found, use the ID as string
+            name = str(decimal_to_hex(number))  # If long name not found, use the ID as string
     return name
 
+
 def get_node_list():
-    #checks nodeDB on device and returns a list of the last 5 nodes heard
+    # checks nodeDB on device and returns a list of the last 5 nodes heard
     node_list = []
     if interface.nodes:
         for node in interface.nodes.values():
-            #ignore own
+            # ignore own
             if node['num'] != myNodeNum:
                 node_name = get_name_from_number(node['num'])
 
@@ -225,85 +239,88 @@ def get_node_list():
                     last_heard = node['lastHeard']
                 except Exception as e:
                     last_heard = 0
-                
-                item = (node_name,last_heard)
+
+                item = (node_name, last_heard)
                 node_list.append(item)
-        
+
         node_list.sort(key=lambda x: x[1], reverse=True)
-        #print (f"Node List: {node_list[:5]}\n")
-        #return only the last 5 nodes
+        # print (f"Node List: {node_list[:5]}\n")
+        # return only the last 5 nodes
         nice_node_list = [x[0] for x in node_list[:5]]
         return nice_node_list
     else:
         node_list.append("Nothing heard")
         return node_list
-    
+
+
 def get_node_location(number):
-    #Get the location of a node by its number from nodeDB on device
+    # Get the location of a node by its number from nodeDB on device
     latitude = 0
     longitude = 0
-    position = [0,0]
+    position = [0, 0]
     if interface.nodes:
         for node in interface.nodes.values():
             if number == node['num']:
                 if 'position' in node:
                     latitude = node['position']['latitude']
                     longitude = node['position']['longitude']
-                    print (f"System: location data for {number} is {latitude},{longitude}")
-                    position = [latitude,longitude]
+                    print(f"System: location data for {number} is {latitude},{longitude}")
+                    position = [latitude, longitude]
                     return position
                 else:
-                    print (f"{log_timestamp()} System: No location data for {number}")
+                    print(f"{log_timestamp()} System: No location data for {number}")
                     return position
     else:
-        print (f"{log_timestamp()} System: No nodes found")
+        print(f"{log_timestamp()} System: No nodes found")
         return position
-        
-def send_message(message,ch,nodeid):
-    #if message over 160 characters, split it into multiple messages
+
+
+def send_message(message, ch, nodeid):
+    # if message over 160 characters, split it into multiple messages
     if len(message) > 160:
-        #message_list = [message[i:i+160] for i in range(0, len(message), 160)]
+        # message_list = [message[i:i+160] for i in range(0, len(message), 160)]
         # smarter word split
         split_message = message.split()
         line = ''
         split_len = 160
         message_list = []
         for word in split_message:
-            if len(line+word)<split_len:
+            if len(line + word) < split_len:
                 line += word + ' '
             else:
                 message_list.append(line)
                 line = word + ' '
-        message_list.append(line) #needed add contents of the last 'line' into the list
+        message_list.append(line)  # needed add contents of the last 'line' into the list
 
         for m in message_list:
             if nodeid == 0:
-                #Send to channel
-                print (f"{log_timestamp()} System: Sending Multi-Chunk: {m} To: Channel:{ch}")
-                interface.sendText(text=m,channelIndex=ch)
+                # Send to channel
+                print(f"{log_timestamp()} System: Sending Multi-Chunk: {m} To: Channel:{ch}")
+                interface.sendText(text=m, channelIndex=ch)
             else:
-                #Send to DM
-                print (f"{log_timestamp()} System: Sending Multi-Chunk: {m} To: {get_name_from_number(nodeid)}")
-                interface.sendText(text=m,channelIndex=ch,destinationId=nodeid)
-    else: #message is less than 160 characters
+                # Send to DM
+                print(f"{log_timestamp()} System: Sending Multi-Chunk: {m} To: {get_name_from_number(nodeid)}")
+                interface.sendText(text=m, channelIndex=ch, destinationId=nodeid)
+    else:  # message is less than 160 characters
         if nodeid == 0:
-            #Send to channel
-            print (f"{log_timestamp()} System: Sending: {message} To: Channel:{ch}")
-            interface.sendText(text=message,channelIndex=ch)
+            # Send to channel
+            print(f"{log_timestamp()} System: Sending: {message} To: Channel:{ch}")
+            interface.sendText(text=message, channelIndex=ch)
         else:
-            #Send to DM
-            print (f"{log_timestamp()} System: Sending: {message} To: {get_name_from_number(nodeid)}")
-            interface.sendText(text=message,channelIndex=ch,destinationId=nodeid)
-    
+            # Send to DM
+            print(f"{log_timestamp()} System: Sending: {message} To: {get_name_from_number(nodeid)}")
+            interface.sendText(text=message, channelIndex=ch, destinationId=nodeid)
+
 
 def exit_handler(signum, frame):
     print("\nSystem: Closing Autoresponder")
     interface.close()
-    exit (0)
+    exit(0)
 
-print ("\nMeshtastic Autoresponder MESH Bot CTL+C to exit\n")
+
+print("\nMeshtastic Autoresponder MESH Bot CTL+C to exit\n")
 pub.subscribe(onReceive, 'meshtastic.receive')
-print (f"System: Autoresponder Started for device {get_name_from_number(myNodeNum)}")
+print(f"System: Autoresponder Started for device {get_name_from_number(myNodeNum)}")
 
 while True:
     # Catch CTL+C to exit
