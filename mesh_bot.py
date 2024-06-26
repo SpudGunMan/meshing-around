fe3cd7ae8826855b3f589570f6776bee8d8d3970
David Bures <12524436+PiDiBi@users.noreply.github.com>
2024-06-26 15:26:42 -0700

    fix help message

--- mesh_bot.py
+++ mesh_bot.py
@@ -29,7 +29,7 @@ trap_list = trap_list + trap_list_conditions
 trap_list = trap_list + trap_list_bbs
 
 welcome_message = "MeshBot, here for you like a friend who is not. Try sending: ping @foo  or, help"
-help_message = "Commands are: ack, hfcond, joke, Lheard, moon, motd, ping, solar, sun, tide, whereami, wx, bbshelp"
+help_message = "Commands are: " + ", ".join(trap_list, ', ')
 MOTD = "Thanks for using PongBOT! Have a good day!" # Message of the Day
 RESPOND_BY_DM_ONLY = True # Set to True to respond messages via DM only (keeps the channel clean)
 
