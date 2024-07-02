3ca3d2428b36c5d316eb7de68fb9ec6286dcd88d
SpudGunMan <kellykeeton@yahoo.com>
2024-07-01 22:23:06 -0700

    ping pong

--- mesh_bot.py
+++ mesh_bot.py
@@ -64,7 +64,7 @@ def auto_response(message, snr, rssi, hop, message_from_id):
     elif "testing" in message.lower():
         bot_response = "Testing 1,2,3"
     elif "pong" in message.lower():
-        bot_response = "PING!!"
+        bot_response = "P-I-N-G!!"
     elif "motd" in message.lower():
         #check if the user wants to set the motd by using $
         if "$" in message:
