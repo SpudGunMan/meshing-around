18e82e75ef25fc5dda514aaa1adf26997c51788e
SpudGunMan <kellykeeton@yahoo.com>
2024-07-10 15:29:52 -0700

    Update launch.sh

--- launch.sh
+++ launch.sh
@@ -3,14 +3,18 @@
 # launch.sh
 echo "Launches the bot in a virtual environment"
 
-# activate the virtual environment
-
-#look for venv folder and activate it if found
+# activate the virtual environment if it exists
 if [ -d "venv" ]; then
     source venv/bin/activate
 fi
 
 # launch the application
+cd "$(dirname "$0")"
 
-python mesh_bot.py
-#python pong_bot.py
+if [ "$1" == "pong" ]; then
+    python pong_bot.py
+elif [ "$1" == "mesh" ]; then
+    python mesh_bot.py
+else
+    echo "Please provide a bot to launch (pong/mesh)"
+fi
