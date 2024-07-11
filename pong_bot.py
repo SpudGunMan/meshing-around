988b713b59833b283b969de70f7b126182aa8104
SpudGunMan <kellykeeton@yahoo.com>
2024-07-11 00:39:20 -0700

    Update pong_bot.py

--- pong_bot.py
+++ pong_bot.py
@@ -86,7 +86,7 @@ def auto_response(message, snr, rssi, hop, message_from_id):
     elif "cmd" in message.lower():
         bot_response = help_message
     elif "lheard" in message.lower() or "sitrep" in message.lower():
-        bot_response = "Last 5 nodes heard:\n" + str(get_node_list())
+        bot_response = "Last heard:\n" + str(get_node_list())
     else:
         bot_response = "I'm sorry, I'm afraid I can't do that."
 
