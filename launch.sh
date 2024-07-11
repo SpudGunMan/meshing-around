cc53bd422c10d09e9dfee6b28b356ccac822cbc5
SpudGunMan <kellykeeton@yahoo.com>
2024-07-10 19:16:41 -0700

    Update launch.sh

--- launch.sh
+++ launch.sh
@@ -3,16 +3,9 @@
 # launch.sh
 cd "$(dirname "$0")"
 
-printf "\n\nLaunches the bot in a virtual environment"
-
 # activate the virtual environment if it exists
 if [ -d "venv" ]; then
     source venv/bin/activate
-fi
-
-if [ ! -d "venv" ]; then
-    echo "No virtual environment found. Please run install.sh to set one up"
-    exit 1
 fi
 
 if [ ! -f "config.ini" ]; then
@@ -20,12 +13,10 @@ if [ ! -f "config.ini" ]; then
 fi
 
 # launch the application
-
-
 if [ "$1" == "pong" ]; then
     python pong_bot.py
 elif [ "$1" == "mesh" ]; then
     python mesh_bot.py
 else
-    echo "Please provide a bot to launch (pong/mesh)"
+    printf "\nPlease provide a bot to launch (pong/mesh)"
 fi
