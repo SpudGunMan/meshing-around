89dc8791d0c92c5166ca307648d05e7d39e5be09
SpudGunMan <kellykeeton@yahoo.com>
2024-08-07 21:55:36 -0700

    fix debug

--- modules/system.py
+++ modules/system.py
@@ -201,9 +201,9 @@ def get_node_list(nodeInt=1):
         node_list2.sort(key=lambda x: x[1], reverse=True)
     except Exception as e:
         logger.error(f"System: Error sorting node list: {e}")
-        node_list = ERROR_FETCHING_DATA
         #print (f"Node List1: {node_list1[:5]}\n")
         #print (f"Node List2: {node_list2[:5]}\n")
+        node_list = ERROR_FETCHING_DATA
 
     try:
         # make a nice list for the user
