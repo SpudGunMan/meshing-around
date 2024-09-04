0869b19408708c75bf657d109f15ae04352012b5
SpudGunMan <kellykeeton@yahoo.com>
2024-09-03 23:05:00 -0700

    addTimeAware
    include the current date in the awareness of location

--- modules/llm.py
+++ modules/llm.py
@@ -102,6 +102,7 @@ def llm_query(input, nodeID=0, location_name=None):
     
     response = ""
     result = ""
+    location_name = location_name + f" at the current time of {datetime.now().strftime('%Y-%m-%d %H:%M:%S')}"
     result = chain_prompt_model.invoke({"input": input, "llmModel": llmModel, "userID": nodeID, \
                                         "history": llmChat_history, "context": googleResults, "location_name": location_name})
     #logger.debug(f"System: LLM Response: " + result.strip().replace('\n', ' '))
