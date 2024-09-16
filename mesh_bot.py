c0e7f4c0fad62b1d2e66a3b8aff786c49abc9273
Nestpebble <116762865+Nestpebble@users.noreply.github.com>
2024-09-16 09:04:38 +0100

    Update mesh_bot.py

--- mesh_bot.py
+++ mesh_bot.py
@@ -16,7 +16,7 @@ def auto_response(message, snr, rssi, hop, message_from_id, channel_number, devi
     command_handler = {
         "ping": lambda: handle_ping(message, hop, snr, rssi),
         "pong": lambda: "🏓PING!!",
-        "motd": lambda: handle_motd(message, deviceID),
+        "motd": lambda: handle_motd(message, message_from_id),
         "bbshelp": bbs_help,
         "wxalert": lambda: handle_wxalert(message_from_id, deviceID, message),
         "wxa": lambda: handle_wxalert(message_from_id, deviceID, message),
