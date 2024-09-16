fd68007c41845bf0763aec35dc53bb2e594ee185
Nestpebble <waveoctave@protonmail.com>
2024-09-16 11:31:46 +0100

    sedfsedf

--- mesh_bot.py
+++ mesh_bot.py
@@ -89,11 +89,11 @@ def handle_motd(message, message_from_id):
         MOTD = motd.rstrip()
         logger.debug(f"System: node changed MOTD: ", {message_from_id})
         return "MOTD Set to: " + MOTD
-    elif "$" in message and not str(bbs_admin_list): #no names in bbs admin list
+    elif "$" in message and not str(bbs_admin_list).strip(): #no names in bbs admin list
         motd = message.split("$")[1]
         MOTD = motd.rstrip()
         return "MOTD Set to: " + MOTD
-    elif "$" in message:
+    elif "$" in message and str(bbs_admin_list).strip():
         logger.debug(f"System: node tried to change MOTD: ", {message_from_id})
         return "I can't do that for you"
     else:
