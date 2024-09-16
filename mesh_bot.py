5d2f7dbe8aeeccba21acb65b2ee483d2a696a93d
Nestpebble <116762865+Nestpebble@users.noreply.github.com>
2024-09-16 02:13:10 +0100

    Update mesh_bot.py

--- mesh_bot.py
+++ mesh_bot.py
@@ -79,7 +79,7 @@ def handle_ping(message, hop, snr, rssi):
         else:
             return "🏓PONG, " + hop
 
-def handle_motd(message, deviceID):
+def handle_motd(message, message_from_id):
     global MOTD
     if "$" in message and str(message_from_id) in bbs_admin_list:
         motd = message.split("$")[1]
