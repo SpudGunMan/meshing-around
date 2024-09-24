d87e70f7eea1253658028c6b465f358b3cd80868
SpudGunMan <kellykeeton@yahoo.com>
2024-09-23 19:35:22 -0700

    Update mesh_bot.py

--- mesh_bot.py
+++ mesh_bot.py
@@ -467,7 +467,7 @@ def handle_sun(message_from_id, deviceID, channel_number):
 
 def handle_lheard(nodeid, deviceID):
     # display last heard nodes add to response
-    bot_response = "LastHeard:\n" + str(get_node_list(1))
+    bot_response = + str(get_node_list(1))
     # gather telemetry
     chutil1 = round(interface1.nodes.get(decimal_to_hex(myNodeNum1), {}).get("deviceMetrics", {}).get("channelUtilization", 0), 1)
     airUtilTx = round(interface1.nodes.get(decimal_to_hex(myNodeNum1), {}).get("deviceMetrics", {}).get("airUtilTx", 0), 1)
