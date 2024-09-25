ece531249e61f171c5a5aea621558063bb928b47
SpudGunMan <kellykeeton@yahoo.com>
2024-09-24 19:32:21 -0700

    Update bbsdb_admin.py

--- etc/bbsdb_admin.py
+++ etc/bbsdb_admin.py
@@ -6,42 +6,42 @@ import pickle # pip install pickle
 try:
     with open('../bbsdb.pkl', 'rb') as f:
         bbs_messages = pickle.load(f)
-except:
+except Exception as e:
     try:
         with open('bbsdb.pkl', 'rb') as f:
             bbs_messages = pickle.load(f)
-    except:
+    except Exception as e:
         print ("\nSystem: bbsdb.pkl not found")
 
 try:
     with open('../bbsdm.pkl', 'rb') as f:
         bbs_dm = pickle.load(f)
-except:
+except Exception as e:
     try:
         with open('bbsdm.pkl', 'rb') as f:
             bbs_dm = pickle.load(f)
-    except:
+    except Exception as e:
         print ("\nSystem: bbsdm.pkl not found")
 
 # Game HS tables
 try:
     with open('../lemonade_hs.pkl', 'rb') as f:
        lemon_score = pickle.load(f)
-except:
+except Exception as e:
     try:
         with open('lemonade_hs.pkl', 'rb') as f:
             lemon_score = pickle.load(f)
-    except:
+    except Exception as e:
         print ("\nSystem: lemonade_hs.pkl not found")
 
 try:
     with open('../dopewar_hs.pkl', 'rb') as f:
         dopewar_score = pickle.load(f)
-except:
+except Exception as e:
     try:
         with open('dopewar_hs.pkl', 'rb') as f:
             dopewar_score = pickle.load(f)
-    except:
+    except Exception as e:
         print ("\nSystem: dopewar_hs.pkl not found")
 
 
