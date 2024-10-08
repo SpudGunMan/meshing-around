0619429c0043bcaf395c164fca9841d9d33b1339
SpudGunMan <kellykeeton@yahoo.com>
2024-10-07 23:13:48 -0700

    Update launch.sh

--- launch.sh
+++ launch.sh
@@ -18,7 +18,7 @@ if [ "$1" == "pong" ]; then
 elif [ "$1" == "mesh" ]; then
     python3 mesh_bot.py
 else
-    printf "\nPlease provide a bot to launch (pong/mesh)"
+    printf "\nActivated virtual environment use 'deactivate' to exit\n"
 fi
 
 deactivate
