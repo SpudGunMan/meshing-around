8b62d7056218d2a1a1515348c5204385a9241a62
SpudGunMan <kellykeeton@yahoo.com>
2024-10-13 16:06:10 -0700

    Update golfsim.py

--- modules/games/golfsim.py
+++ modules/games/golfsim.py
@@ -323,7 +323,6 @@ def playGolf(nodeID, message, finishedHole=False):
                 msg += "\nYou have " + str(distance_remaining) + "yd. ⛳️"
                 msg += "\nClub?[D, L, M, H, G, W]🏌️"
 
-                total_strokes += hole_shots
 
                 # save player's current game state, keep stroking
                 for i in range(len(golfTracker)):
