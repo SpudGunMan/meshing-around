c416b00383324b7bc939a8b2f9b0e1918f349f5e
SpudGunMan <kellykeeton@yahoo.com>
2024-11-25 11:49:39 -0800

    newLogRotation

--- etc/report_generator.py
+++ etc/report_generator.py
@@ -922,8 +922,8 @@ def generate_database_html(database_info):
 
 def main():
     log_dir = LOG_PATH
-    today = datetime.now().strftime('%Y_%m_%d')
-    log_file = f'meshbot{today}.log'
+    today = datetime.now().strftime('%Y-%m-%d')
+    log_file = f'meshbot.log.{today}'
     log_path = os.path.join(log_dir, log_file)
 
     if not os.path.exists(log_path):
