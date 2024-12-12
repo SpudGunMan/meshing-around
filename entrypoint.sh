517ae5d4b4387211a48180609738340cdf3ea2ee
SpudGunMan <kellykeeton@yahoo.com>
2024-12-11 17:14:28 -0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+# instruction set the meshing-around docker container
 
 # Substitute environment variables in the config file
 envsubst < /app/config.ini > /app/config.tmp && mv /app/config.tmp /app/config.ini
