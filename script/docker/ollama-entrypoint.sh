df15fb54b02acf8c0acebcf2d1e5deb339d34fca
Ryan Turner <turnrye@proton.me>
2025-01-04 19:39:23 -0600

    Initial checkin

---
+++ script/docker/ollama-entrypoint.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Start Ollama in the background.
+/bin/ollama serve &
+# Record Process ID.
+pid=$!
+
+# Pause for Ollama to start.
+sleep 5
+
+echo "🔴 Retrieve llama3.2:3b model..."
+ollama pull llama3.2:3b
+echo "🟢 Done!"
+
+# Wait for Ollama process to finish.
+wait $pid
