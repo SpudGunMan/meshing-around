921b66f9e19b57ee4a632ff8e64d19239a70abe9
SpudGunMan <kellykeeton@yahoo.com>
2025-01-12 10:12:06 -0800

    Update entrypoint.sh

--- script/docker/entrypoint.sh
+++ script/docker/entrypoint.sh
@@ -2,6 +2,6 @@
 # instruction set the meshing-around docker container
 
 # Substitute environment variables in the config file
-envsubst < /app/config.ini > /app/config.tmp && mv /app/config.tmp /app/config.ini
+#envsubst < /app/config.ini > /app/config.tmp && mv /app/config.tmp /app/config.ini
 
 exec python /app/mesh_bot.py
