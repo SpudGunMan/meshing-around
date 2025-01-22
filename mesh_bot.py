73e8e063d268489b86ab667e5ebfaec622c73f82
SpudGunMan <kellykeeton@yahoo.com>
2025-01-21 20:22:25 -0800

    Update mesh_bot.py

--- mesh_bot.py
+++ mesh_bot.py
@@ -397,7 +397,7 @@ def handle_llm(message_from_id, channel_number, deviceID, message, publicChannel
     # information for the user on how long the query will take on average
     if llmRunCounter > 0:
         averageRuntime = sum(llmTotalRuntime) / len(llmTotalRuntime)
-        msg = f"Please wait, average query time is: {int(averageRuntime)} seconds" if averageRuntime > 25 else ''
+        msg = f"Average query time is: {int(averageRuntime)} seconds" if averageRuntime > 25 else ''
     else:
         msg = "Please wait, response could take 30+ seconds. Fund the SysOp's GPU budget!"
 
