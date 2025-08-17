f04392a81cc63654708e60a1d722faf851ebe515
SpudGunMan <kellykeeton@yahoo.com>
2025-08-16 20:25:53 -0700

    Update llm.py

--- modules/llm.py
+++ modules/llm.py
@@ -27,9 +27,9 @@ llmChat_history = {}
 trap_list_llm = ("ask:", "askai")
 
 meshbotAIinit = """
-    You must keep responses under 450 characters at all times, the response will be cut off if it exceeds this limit.
+    You must keep your responses under 450 tokens
+    You can not ask for clarification, you must respond to the prompt as if you are a chatbot assistant.
     You must respond in plain text standard ASCII characters, or emojis.
-    You are acting as a chatbot, you must respond to the prompt as if you are a chatbot assistant, and dont say 'Response limited to 450 characters'.
     """
 
 meshBotAI = """
