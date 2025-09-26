a80f926d08bff59144c81b3bef732271c6381f3f
SpudGunMan <kellykeeton@yahoo.com>
2025-09-26 15:43:29 -0700

    Update launch.sh

--- launch.sh
+++ launch.sh
@@ -26,6 +26,8 @@ elif [ "$1" == "html" ]; then
     python3 etc/report_generator.py
 elif [ "$1" == "html5" ]; then
     python3 etc/report_generator5.py
+elif [ "$1" == "addfav" ]; then
+    python3 script/addFav.py
 else
     echo "Please provide a bot to launch (pong/mesh) or a report to generate (html/html5)"
     exit 1
