685bd3491d8ec9201bd0ef2875af8ffeda5cf2f6
SpudGunMan <kellykeeton@yahoo.com>
2025-10-17 17:10:44 -0700

    Update udp.py

--- modules/udp.py
+++ modules/udp.py
@@ -6,7 +6,7 @@
 # 2025 Kelly Keeton K7MHI
 from pubsub import pub
 from meshtastic.protobuf import mesh_pb2, portnums_pb2
-from mudp import UDPPacketStream, node, conn, send_text_message, send_nodeinfo, send_device_telemetry, send_position, send_environment_metrics, send_power_metrics, send_waypoint
+from mudp import UDPPacketStream, node, conn, send_text_message, send_nodeinfo, send_device_telemetry, send_position, send_environment_metrics, send_power_metrics, send_waypoint, send_data
 import time
 from zeroconf import Zeroconf, ServiceBrowser
 
