3540b8f110de204ced001ba4dc3c07260893152c
SpudGunMan <kellykeeton@yahoo.com>
2025-10-23 23:10:15 -0700

    Update entrypoint.sh

--- script/docker/entrypoint.sh
+++ script/docker/entrypoint.sh
@@ -3,9 +3,7 @@
 # if no config.ini exists, copy the default one
 if [ ! -f /app/config.ini ]; then
     cp /app/config.template /app/config.ini
-fi
-# if using docker, set interface type to tcp and hostname to meshtasticd:4403
-if [ -f /app/config.ini ]; then
+
     ls -l /app/config.ini
     # Set type = tcp in [interface]
     sed -i '/^\[interface\]/,/^[^[]/ s/^type = .*/type = tcp/' /app/config.ini
