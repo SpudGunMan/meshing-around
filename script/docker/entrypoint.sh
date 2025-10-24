48788ceda8a22e5e23fddb51a91156d61850c76c
SpudGunMan <kellykeeton@yahoo.com>
2025-10-23 22:57:50 -0700

    Update entrypoint.sh

--- script/docker/entrypoint.sh
+++ script/docker/entrypoint.sh
@@ -3,7 +3,10 @@
 # if no config.ini exists, copy the default one
 if [ ! -f /app/config.ini ]; then
     cp config.template config.ini
-    sed -i '' -e '/^\[interface\]/,/^[^[]/ s/type = .*/type = tcp/' -e '/^\[interface\]/,/^[^[]/ s/hostname = .*/hostname = localhost:4403/' config.ini
+fi
+# if using docker, set interface type to tcp and hostname to meshtasticd:4403
+if [ "$DOCKER_ENV" = "true" ]; then
+    sed -i '' -e '/^\[interface\]/,/^[^[]/ s/type = .*/type = tcp/' -e '/^\[interface\]/,/^[^[]/ s/hostname = .*/hostname = meshtasticd:4403/' config.ini
 fi
 
 # Run the bot as appuser (if you want to drop privileges)
