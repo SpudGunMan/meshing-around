98af757d9332cc454b64135a8b8bc3ae7af0a0bf
SpudGunMan <kellykeeton@yahoo.com>
2025-10-23 21:50:54 -0700

    enhance

--- script/docker/entrypoint.sh
+++ script/docker/entrypoint.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
 
+# if no config.ini exists, copy the default one
+if [ ! -f /app/config.ini ]; then
+    cp /app/script/docker/config.ini /app/config.ini
+fi
+
 # Run the bot as appuser (if you want to drop privileges)
 exec python /app/mesh_bot.py
