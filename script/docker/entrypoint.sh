3fdebf3bf96097e1ed1f6118338eb9ddf459fe26
SpudGunMan <kellykeeton@yahoo.com>
2025-10-23 23:07:54 -0700

    Update entrypoint.sh

--- script/docker/entrypoint.sh
+++ script/docker/entrypoint.sh
@@ -7,8 +7,12 @@ fi
 # if using docker, set interface type to tcp and hostname to meshtasticd:4403
 if [ -f /app/config.ini ]; then
     ls -l /app/config.ini
-    sed -i '/^\[interface\]/,/^[^[]/ s/type = .*/type = tcp/' /app/config.ini
-    sed -i '/^\[interface\]/,/^[^[]/ s/hostname = .*/hostname = meshtasticd:4403/' /app/config.ini
+    # Set type = tcp in [interface]
+    sed -i '/^\[interface\]/,/^[^[]/ s/^type = .*/type = tcp/' /app/config.ini
+    # Remove any commented or uncommented hostname lines in [interface]
+    sed -i '/^\[interface\]/,/^[^[]/ s/^#\? *hostname = .*$//' /app/config.ini
+    # Add hostname = meshtasticd:4403 after [interface]
+    sed -i '/^\[interface\]/a hostname = meshtasticd:4403' /app/config.ini
 fi
 # Run the bot as appuser (if you want to drop privileges)
 exec python /app/mesh_bot.py
