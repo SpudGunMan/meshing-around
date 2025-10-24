d8839275727c648cfe53b551aac37a44f80d3162
SpudGunMan <kellykeeton@yahoo.com>
2025-10-23 21:57:50 -0700

    enhance

--- script/docker/entrypoint.sh
+++ script/docker/entrypoint.sh
@@ -2,7 +2,7 @@
 
 # if no config.ini exists, copy the default one
 if [ ! -f /app/config.ini ]; then
-    cp /app/script/docker/config.ini /app/config.ini
+    cp /app/script/docker/config.template /app/config.ini
 fi
 
 # Run the bot as appuser (if you want to drop privileges)
