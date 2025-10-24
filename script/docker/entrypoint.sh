17c693c2f7f51b6c376379f208d48056e83197aa
SpudGunMan <kellykeeton@yahoo.com>
2025-10-23 22:55:16 -0700

    Update entrypoint.sh

--- script/docker/entrypoint.sh
+++ script/docker/entrypoint.sh
@@ -3,7 +3,7 @@
 # if no config.ini exists, copy the default one
 if [ ! -f /app/config.ini ]; then
     cp config.template config.ini
-    sed -i '' -e '/^\[interface\]/,/^[^[]/ s/type = .*/type = tcp/' -e '/^\[interface\]/,/^[^[]/ s/hostname = .*/hostname = localhost:4403/' script/docker/config.ini
+    sed -i '' -e '/^\[interface\]/,/^[^[]/ s/type = .*/type = tcp/' -e '/^\[interface\]/,/^[^[]/ s/hostname = .*/hostname = localhost:4403/' config.ini
 fi
 
 # Run the bot as appuser (if you want to drop privileges)
