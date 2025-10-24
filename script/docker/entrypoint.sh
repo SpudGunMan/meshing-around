c6b5a1c7087f508fd8d124ea11cc9538c7b09fb4
SpudGunMan <kellykeeton@yahoo.com>
2025-10-23 21:26:32 -0700

    docker
    i never enjoy docker

--- script/docker/entrypoint.sh
+++ script/docker/entrypoint.sh
@@ -1,13 +1,4 @@
 #!/bin/bash
 
-# if /app/logs is not writable, change ownership
-if [ -w /app/logs ]; then
-    chown -R appuser:appuser /app/logs
-fi
-# if /app/data is not writable, change ownership
-if [ -w /app/data ]; then
-    chown -R appuser:appuser /app/data
-fi
-
 # Run the bot as appuser (if you want to drop privileges)
 exec python /app/mesh_bot.py
