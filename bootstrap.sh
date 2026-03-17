f1e1516919643b32da62c53f9913e3ebe85309d5
Kelly <kellykeeton@yahoo.com>
2026-03-17 16:18:46 -0700

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -3,13 +3,25 @@ set -e
 BASE_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
 cd "$BASE_DIR"
-python3 -m venv "$BASE_DIR/venv"
+
+if [[ ! -d "$BASE_DIR/venv" ]]; then
+	python3 -m venv "$BASE_DIR/venv"
+fi
+
 source "$BASE_DIR/venv/bin/activate"
 "$BASE_DIR/venv/bin/pip" install -r "$BASE_DIR/requirements.txt"
-cp -r "$BASE_DIR/etc/data/." "$BASE_DIR/data/"
-cp "$BASE_DIR/etc/config.template" "$BASE_DIR/config.ini"
-replace="s|type = serial|type = tcp|g"
-sed -i '' "$replace" "$BASE_DIR/config.ini"
-replace="s|# hostname = meshtastic.local|hostname = localhost|g"
-sed -i '' "$replace" "$BASE_DIR/config.ini"
+
+mkdir -p "$BASE_DIR/data"
+cp -Rn "$BASE_DIR/etc/data/." "$BASE_DIR/data/"
+
+if [[ ! -f "$BASE_DIR/config.ini" ]]; then
+	cp "$BASE_DIR/etc/config.template" "$BASE_DIR/config.ini"
+	replace="s|type = serial|type = tcp|g"
+	sed -i '' "$replace" "$BASE_DIR/config.ini"
+	replace="s|# hostname = meshtastic.local|hostname = localhost|g"
+	sed -i '' "$replace" "$BASE_DIR/config.ini"
+else
+	echo "config.ini already exists, leaving it unchanged."
+fi
+
 deactivate
